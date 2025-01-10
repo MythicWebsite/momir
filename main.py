f83710856121932bba498d3ba0e88101fafde337
Mythic <mythicwebsite@gmail.com>
2025-01-10 11:13:59 -0500

    .

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from pprint import pprint
 from api_handler import get_creature_card_list, download_img
 from image_handler import convert_card
 import random
-
+import os
 
 creatures = get_creature_card_list()
 debug_count = 0
 
 card = random.choice(creatures['3'])
-
-print(f"Processing {card['name']}")
-img_url = card['image_uris']['border_crop']
-convert_card(download_img(img_url), card['id'])
+print(f'Selected random {int(card["cmc"])} cmc card: {card["name"]}')
+if not os.path.exists(f'Images/{card["id"]}.png'):
+    print(f"Creating image for {card['name']}")
+    img_url = card['image_uris']['border_crop']
+    convert_card(download_img(img_url), card['id'])
 # for card in creatures['1']:
 #     print(f"Processing {card['name']}")
 #     img_url = card['image_uris']['border_crop']
